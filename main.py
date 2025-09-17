9c2055954a1a67496e49b774854848fe58877a6b
jokker5502 <matecracksuperpro123@gmail.com>
2025-09-17 17:54:25 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ class Estudiante(BaseModel):
     nombre: str
     edad: int
 
-
+# Base de datos simulada
 db_estudiantes = [
     Estudiante(id=1, nombre="Ana Gómez", edad=21),
     Estudiante(id=2, nombre="Carlos Ruiz", edad=23),
@@ -23,8 +23,7 @@ app = FastAPI(
 @app.get("/estudiantes/", response_model=List[Estudiante])
 def get_todos_los_estudiantes():
     """
-    Esta función se ejecuta cuando alguien visita /estudiantes/
-    y simplemente devuelve la lista completa de 'db_estudiantes'.
+    Devuelve todos los estudiantes en la base de datos simulada.
     """
     return db_estudiantes
 
@@ -32,8 +31,7 @@ def get_todos_los_estudiantes():
 @app.get("/estudiantes/{estudiante_id}", response_model=Estudiante)
 def get_estudiante_por_id(estudiante_id: int):
     """
-    Esta función se ejecuta cuando alguien visita /estudiantes/1, /estudiantes/2, etc.
-    Busca en la lista 'db_estudiantes' al estudiante que tenga el ID correcto.
+    Devuelve un estudiante por su ID.
     """
     for estudiante in db_estudiantes:
         if estudiante.id == estudiante_id:
@@ -42,11 +40,23 @@ def get_estudiante_por_id(estudiante_id: int):
     raise HTTPException(status_code=404, detail="Estudiante no encontrado")
 
 
+@app.put("/estudiantes/{estudiante_id}", response_model=Estudiante)
+def update_estudiante(estudiante_id: int, estudiante_actualizado: Estudiante):
+    """
+    Actualiza los datos de un estudiante por su ID.
+    """
+    for i, estudiante in enumerate(db_estudiantes):
+        if estudiante.id == estudiante_id:
+            db_estudiantes[i] = estudiante_actualizado
+            return estudiante_actualizado
+    
+    raise HTTPException(status_code=404, detail="Estudiante no encontrado")
+
+
 @app.delete("/estudiantes/{estudiante_id}", response_model=Estudiante)
 def delete_estudiante(estudiante_id: int):
     """
-    Esta función se ejecuta cuando alguien visita /estudiantes/{id} con el método DELETE.
-    Elimina al estudiante con el ID proporcionado de la lista 'db_estudiantes'.
+    Elimina un estudiante de la base de datos por su ID.
     """
     for estudiante in db_estudiantes:
         if estudiante.id == estudiante_id:
