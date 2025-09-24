f235253665c93738db113d6655dc7c7cd13b8b61
Mateo <matecracksuperpro123@gmail.com>
2025-09-24 17:40:51 -0500

    cambio final en usuario

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # --- URL de Conexión a la Base de Datos ---
-DB_USER = os.getenv("DB_USER", "mateo")
+DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "uide.asu.123")
 DB_HOST = os.getenv("DB_HOST", "mi-api-database.c07yeca6uwgy.us-east-1.rds.amazonaws.com")
 DB_NAME = os.getenv("DB_NAME", "postgres")
